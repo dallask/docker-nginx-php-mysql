85de1703452086fe06edaa1505255d8e7ad06a18
Ievgen Kyvgyla <Ievgen_Kyvgyla@epam.com>
2018-06-21 16:38:50 +0300

    Indexation when keys are omitted
    Keys conversions
    whats-the-difference-between-array-merge-and-array-array

---
+++ web/public/keys.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ievgen_Kyvgyla
+ * Date: 21-Jun-18
+ * Time: 11:28
+ */
+
+#2 Пример преобразования типов и перезаписи элементов
+$array = array(
+    1    => "a",
+    "1"  => "b",
+    1.5  => "c",
+    true => "d",
+);
+var_dump($array);
---
+++ web/public/whats-the-difference-between-array-merge-and-array-array.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ievgen_Kyvgyla
+ * Date: 21-Jun-18
+ * Time: 15:22
+ */
+
+$a = array('apple');
+$b = array('orange', 'lemon');
+
+echo '$a + $b = ';
+print_r($a + $b);
+
+echo 'array_merge($a, $b) = ';
+print_r(array_merge($a, $b));
